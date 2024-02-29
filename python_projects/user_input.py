48dc883e3e5011560a1132844ba4c190c317fb41
siedd <saidmwacharo8@gmail.com>
2024-02-29 17:42:24 +0300

    input

--- python_projects/user_input.py
+++ python_projects/user_input.py
@@ -1,1 +1,7 @@
+name = input("What is your name?")
+age = input("What is your age?")
+
+location = input("What is your location?")
+
+print(f"My name is {name}, I am {age} years old and live in {location}.")
 
