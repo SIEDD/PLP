9d30a6b613cfc1aafb9a9b3463fc0d5aeade9e40
siedd <saidmwacharo8@gmail.com>
2024-03-06 14:23:58 +0300

    my list week 2

---
+++ python_projects/my_list.py
@@ -0,0 +1,14 @@
+my_list = []
+
+my_list.extend([10, 20, 30, 40])
+
+my_list.insert(1, 15)
+
+my_list.extend([50, 60, 70])
+
+my_list.pop()
+
+my_list.sort()
+
+numbers = my_list.index(30)
+print("Index of value 30 in my_list:", numbers)
