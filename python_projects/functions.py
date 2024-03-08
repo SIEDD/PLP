5ee94979918bdf85cf83f0231e8eee890c96571c
siedd <saidmwacharo8@gmail.com>
2024-03-08 10:00:24 +0300

    functions

--- python_projects/functions.py
+++ python_projects/functions.py
@@ -1,1 +1,17 @@
+def calculate_discount(price, discount_percent):
+    if discount_percent >= 20:
+        discounted_price = price - (price * discount_percent / 100)
+        return discounted_price
+    else:
+        return price
+
+original_price = float(input("Enter the original price of the item: "))
+discount_percent = float(input("Enter the discount percentage: "))
+
+final_price = calculate_discount(original_price, discount_percent)
+
+if final_price != original_price:
+    print("Final price after applying {}% discount: ${:.2f}".format(discount_percent, final_price))
+else:
+    print("No discount applied. Original price: ${:.2f}".format(original_price))
 
